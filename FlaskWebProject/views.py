06ef5a2c305fdaba8d108d1ed49e0228e7e2a0ad
Gautam Kanumuru <gautamka@microsoft.com>
2016-03-20 22:40:22 -0700

    Trying to change views.py file of new repository to FlaskServer.py file of previous repository - direct port

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,37 +1,95 @@
-"""
-Routes and views for the flask application.
-"""
+# Import the Flask Framework
+from flask import Flask, render_template, redirect, request
+from google.appengine.api import mail
 
-from datetime import datetime
-from flask import render_template
-from FlaskWebProject import app
+app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+def hello():
+    """Return a friendly HTTP greeting."""
+    return render_template('MainPage.html')
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
+@app.route('/aboutme')
+def aboutMe():
+	return render_template('AboutMe.html')
 
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+@app.route('/testing')
+def testing():
+	return render_template('MainPageSeptember6th2014.html')
+
+@app.route('/aboutsite')
+def aboutSite():
+	return render_template('AboutSite.html')
+
+@app.route('/timduncan')
+def timDuncan():
+	return render_template('TimDuncan.html')
+
+@app.route('/timduncansignup', methods=['POST'])
+def timDuncanSignUp():
+	text = request.form['text']
+	if(text.count("@") != 0):
+		mail.send_mail(sender="Tim Duncan <venkata.gautam@gmail.com>",
+              to="Gautam <gautamkwebsite@gmail.com>",
+              subject="Add someone to Tim Duncan List",
+              body=text)
+		return redirect("http://www.gautamk.us", code=302)
+	return redirect("http://www.gautamk.us/timduncan", code=302)
+
+@app.route('/timduncanquit', methods=['POST'])
+def timDuncanQuit():
+	text = request.form['textQuit']
+	if(text.count("@") != 0):
+		mail.send_mail(sender="Tim Duncan <venkata.gautam@gmail.com>",
+              to="Gautam <gautamkwebsite@gmail.com>",
+              subject="Unsubscribe someone from Tim Duncan List",
+              body=text)
+		return redirect("http://www.gautamk.us", code=302)
+	return redirect("http://www.gautamk.us/timduncan", code=302)
+
+@app.route('/projects')
+def projects():
+	return render_template('Publications.html')
+
+@app.route('/pubs')
+def publications():
+	return render_template('Publications.html')
+
+@app.route('/tracking')
+def tracker():
+	return render_template('tracker.html')
+
+@app.route('/nasa')
+def nasa():
+	return redirect("http://www.seas.virginia.edu/pubs/spectra/pdfs/nasapartnerships.pdf", code=302)
+
+@app.route('/resume')
+def resume():
+	return redirect("https://s3.amazonaws.com/GautamResume/GautamKanumuruResume.pdf", code=302)
+
+@app.route('/uvradiationabstract')
+def uvabstract():
+	return redirect("https://s3.amazonaws.com/GautamResume/UVAbstract.pdf", code=302)
+
+@app.route('/uvradiationpaper')
+def uvpaper():
+	return redirect("https://s3.amazonaws.com/GautamResume/UVPaper.pdf", code=302)
+
+@app.route('/fieabstract')
+def fieabstract():
+	return redirect("https://s3.amazonaws.com/GautamResume/FIEAbstract.pdf", code=302)
+
+@app.route('/fiepaper')
+def fiepaper():
+	return redirect("https://s3.amazonaws.com/GautamResume/FIEPaper.pdf", code=302)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Custom 404 Page."""
+    return render_template('ErrorPage.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    """Custom 500 Page."""
+    return render_template('500Error.html'), 500
