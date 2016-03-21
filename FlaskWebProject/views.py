5efd25368d38bf79bfe7d3c0317f9ea84d4e795d
Gautam Kanumuru <gautamka@microsoft.com>
2016-03-20 22:51:09 -0700

    Added normal homepage as second iteration

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -10,11 +10,12 @@ from FlaskWebProject import app
 @app.route('/home')
 def home():
     """Renders the home page."""
-    return render_template(
+    '''return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
-    )
+    )'''
+	return render_template('MainPage.html')
 
 @app.route('/contact')
 def contact():
