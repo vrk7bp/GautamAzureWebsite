3cc7acfd474f22fdd4b51613e564606472658768
Gautam Kanumuru <gautamka@microsoft.com>
2016-03-26 21:40:00 -0700

    Added import statement at top of Flask Server

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -4,6 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
+from flask import redirect
 from FlaskWebProject import app
 
 @app.route('/')
